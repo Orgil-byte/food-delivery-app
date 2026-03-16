49eea161b6dbb6e79818094fcda74a7be30328da
U.Orgil <ulziitogtohorgil1908@gmail.com>
2026-03-16 11:41:07 +0800

    A

--- src/controllers/foodOrder/post-order.ts
+++ src/controllers/foodOrder/post-order.ts
@@ -1,21 +1,39 @@
 import { Request, Response } from "express";
 import prisma from "../../lib/prisma";
 
+type OrderItem = {
+  foodId: number;
+  quantity: number;
+  price: number;
+};
+
+type BodyType = {
+  userId: number;
+  foodOrderItems: OrderItem[];
+};
+
 export const postOrder = async (req: Request, res: Response) => {
-  const { userId, foodOrderItems } = req.body;
+  const { userId, foodOrderItems }: BodyType = req.body;
+
+  const totalPrice = foodOrderItems
+    .map((item) => {
+      return item.quantity * item.price;
+    })
+    .reduce((acc, currentPrice) => {
+      acc += currentPrice;
+      return acc;
+    }, 0);
+
+  const foodMap = foodOrderItems.map((item) => ({
+    foodId: item.foodId,
+    quantity: item.quantity,
+  }));
 
   const order = await prisma.foodOrder.create({
     data: {
       userId,
       totalPrice,
-      foodOrderItems: {
-        create: foodOrderItems.map(
-          (item: { foodId: number; quantity: number; price: number }) => ({
-            foodId: item.foodId,
-            quantity: item.quantity,
-          }),
-        ),
-      },
+      foodOrderItems: { create: foodMap },
     },
     include: {
       foodOrderItems: {
--- src/controllers/foodOrder/put-order.ts
+++ src/controllers/foodOrder/put-order.ts
@@ -3,11 +3,11 @@ import prisma from "../../lib/prisma";
 
 export const putOrder = async (req: Request, res: Response) => {
   const id = Number(req.params["id"]);
-  const { status } = req.body;
+  const { status, foodOrderItems } = req.body;
 
   const order = await prisma.foodOrder.update({
     where: { id },
-    data: { status },
+    data: { status, foodOrderItems },
   });
 
   if (!order) {
