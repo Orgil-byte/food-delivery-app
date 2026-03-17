569e251ac8a75d0d0b620252d657ab86c49cd367
U.Orgil <ulziitogtohorgil1908@gmail.com>
2026-03-17 11:08:10 +0800

    A

--- src/controllers/users/post-user.ts
+++ src/controllers/users/post-user.ts
@@ -2,7 +2,29 @@ import { Request, Response } from "express";
 import prisma from "../../lib/prisma";
 
 export const addUsers = async (req: Request, res: Response) => {
-  const { name, email } = req.body;
-  const users = await prisma.user.create({ data: { name, email } });
+  const {
+    name,
+    email,
+    password,
+    phoneNumber,
+    address,
+    role,
+    orderedFoods,
+    isVerified,
+    // ttl,
+  } = req.body;
+  const users = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password,
+      phoneNumber,
+      address,
+      role,
+      orderedFoods,
+      isVerified,
+      // ttl,
+    },
+  });
   res.status(201).json({ users });
 };
--- src/controllers/users/put-user.ts
+++ src/controllers/users/put-user.ts
@@ -3,10 +3,30 @@ import prisma from "../../lib/prisma";
 
 export const updateUsers = async (req: Request, res: Response) => {
   const id = Number(req.params["id"]);
-  const { name, email } = req.body;
+  const {
+    name,
+    email,
+    password,
+    phoneNumber,
+    address,
+    role,
+    orderedFoods,
+    isVerified,
+    // ttl,
+  } = req.body;
   const users = await prisma.user.update({
     where: { id },
-    data: { name, email },
+    data: {
+      name,
+      email,
+      password,
+      phoneNumber,
+      address,
+      role,
+      orderedFoods,
+      isVerified,
+      // ttl,
+    },
   });
   if (!users) {
     res.status(404).json({ error: "User is not on the list" });
