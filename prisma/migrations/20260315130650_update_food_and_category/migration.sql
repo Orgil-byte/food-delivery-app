d18a1ab2e621ae836afcc887525b78d5ea36ac0c
U.Orgil <ulziitogtohorgil1908@gmail.com>
2026-03-17 12:17:20 +0800

    A

--- prisma/migrations/20260315130650_update_food_and_category/migration.sql
+++ prisma/migrations/20260315130650_update_food_and_category/migration.sql
@@ -17,16 +17,3 @@ ADD COLUMN     "image" TEXT NOT NULL,
 ADD COLUMN     "ingredients" TEXT NOT NULL,
 ADD COLUMN     "price" DOUBLE PRECISION NOT NULL,
 ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
-
--- CreateTable
-CREATE TABLE "FoodCategory" (
-    "id" SERIAL NOT NULL,
-    "categoryName" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "FoodCategory_pkey" PRIMARY KEY ("id")
-);
-
--- AddForeignKey
-ALTER TABLE "Food" ADD CONSTRAINT "Food_foodCategoryId_fkey" FOREIGN KEY ("foodCategoryId") REFERENCES "FoodCategory"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
