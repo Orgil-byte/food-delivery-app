383ad9e1245f9321d1c19854457f6b502011823b
U.Orgil <ulziitogtohorgil1908@gmail.com>
2026-03-17 11:29:03 +0800

    a

--- src/routes/food-order.routes.ts
+++ src/routes/food-order.routes.ts
@@ -1,15 +1,15 @@
 import { Router } from "express";
 import { getOrders } from "../controllers/foodOrder/get-food-order";
 import { getOrderById } from "../controllers/foodOrder/get-order-by-id";
-import { addOrder } from "../controllers/foodOrder/post-order";
-import { updateOrder } from "../controllers/foodOrder/put-order";
+import { addOrder } from "../controllers/foodOrder/add-order";
+import { updateOrder } from "../controllers/foodOrder/update-order";
 import { deleteOrder } from "../controllers/foodOrder/delete-order";
 
 const router = Router();
 
 router.get("/", getOrders);
 router.get("/:id", getOrderById);
-router.post("/", updateOrder);
+router.post("/", addOrder);
 router.put("/:id", updateOrder);
 router.delete("/:id", deleteOrder);
 
