46311b2d13263cce3d11d2fed44ad9ac502ad9c9
U.Orgil <ulziitogtohorgil1908@gmail.com>
2026-03-17 11:27:38 +0800

    :A:

--- src/routes/foods.routes.ts
+++ src/routes/foods.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import { getFoods } from "../controllers/foods/get-foods";
 import { getFoodById } from "../controllers/foods/get-food-by-id";
-import { addFood } from "../controllers/foods/post-food";
-import { updateFood } from "../controllers/foods/put-foods";
+import { addFood } from "../controllers/foods/add-food";
+import { updateFood } from "../controllers/foods/update-foods";
 import { deleteFood } from "../controllers/foods/delete-food";
 
 const router = Router();
