5c295054f8f9c59e4483f1925c0c988e44b5d710
U.Orgil <ulziitogtohorgil1908@gmail.com>
2026-03-18 10:17:13 +0800

    :A:

--- src/controllers/users/get-user-by-id.ts
+++ src/controllers/users/get-user-by-id.ts
@@ -3,7 +3,12 @@ import prisma from "../../lib/prisma";
 
 export const getUsersById = async (req: Request, res: Response) => {
   const id = Number(req.params["id"]);
-  const user = await prisma.user.findUnique({ where: { id } });
+  const user = await prisma.user.findUnique({
+    where: { id },
+    include: {
+      foodOrders: true,
+    },
+  });
   if (!user) {
     res.status(404).json({
       error: "User is not on the list",
--- src/controllers/users/get-user.ts
+++ src/controllers/users/get-user.ts
@@ -2,6 +2,10 @@ import { Request, Response } from "express";
 import prisma from "../../lib/prisma";
 
 export const getUsers = async (_req: Request, res: Response) => {
-  const users = await prisma.user.findMany();
+  const users = await prisma.user.findMany({
+    include: {
+      foodOrders: true,
+    },
+  });
   res.json({ users });
 };
