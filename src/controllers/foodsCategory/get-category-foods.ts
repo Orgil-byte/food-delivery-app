097af131249684bda9e0268e53e9c1361f61616b
U.Orgil <ulziitogtohorgil1908@gmail.com>
2026-03-20 11:48:22 +0800

    A

--- src/controllers/foodsCategory/get-category-foods.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-import prisma from "../../lib/prisma";
-
-export const getCategory = async (req: Request, res: Response) => {
-  const foodCat = await prisma.foodCategory.findMany({
-    include: { foods: true },
-  });
-  res.json({ foodCat });
-};
--- src/routes/login.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { authUsers } from "../controllers/users/authentication/auth";
-
-const router = Router();
-
-router.post("/login", authUsers);
-
-export default router;
