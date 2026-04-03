49301bc98663d725ec42d43285a714c80c15de22
U.Orgil <ulziitogtohorgil1908@gmail.com>
2026-04-03 10:17:12 +0800

    A

---
+++ server/api/index.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import foodsRouter from "../src/routes/foods.routes";
+import usersRouter from "../src/routes/users.routes";
+import foodCategoryRouter from "../src/routes/foodsCateg.routes";
+import foodOrderRouter from "../src/routes/food-order.routes";
+import loginRouter from "../src/routes/login.routes";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
+
+app.use(express.json());
+app.use("/foodCateg", foodCategoryRouter);
+app.use("/foods", foodsRouter);
+app.use("/users", usersRouter);
+app.use("/foodOrder", foodOrderRouter);
+app.use("/users", loginRouter);
+
+export default app;
