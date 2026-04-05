6a1a7a719721ab18275d5183954d53177f071032
Orgil-byte <ulziitogtohorgil1908@gmail.com>
2026-04-05 13:20:38 +0800

    A

--- server/api/index.ts
+++ server/api/index.ts
@@ -10,7 +10,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://admin-food-real-one.vercel.app/",
+    origin: [
+      "https://admin-food-real-one.vercel.app/",
+      "http://localhost:3000/",
+    ],
   }),
 );
 
