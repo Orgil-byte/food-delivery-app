c8e6802eb3fd12fef8375d540fe03733ea27cf4b
U.Orgil <ulziitogtohorgil1908@gmail.com>
2026-04-07 11:55:24 +0800

    A

--- client/app/_components/foodOrder.tsx
+++ client/app/_components/foodOrder.tsx
@@ -1,9 +1,13 @@
+import { DialogTrigger, DialogContent, Dialog } from "@/components/ui/dialog";
 import { Plus } from "lucide-react";
 
 export const FoodOrder = () => {
   return (
-    <div className="absolute bottom-6 transition-transform duration-150 hover:scale-110 right-6 w-11 h-11 rounded-full bg-white flex justify-center items-center cursor-pointer">
-      <Plus strokeWidth={2} size={16} className="text-red-500" />
-    </div>
+    <Dialog>
+      <DialogTrigger className="absolute bottom-6 transition-transform duration-150 hover:scale-110 right-6 w-11 h-11 rounded-full bg-white flex justify-center items-center cursor-pointer">
+        <Plus strokeWidth={2} size={16} className="text-red-500" />
+      </DialogTrigger>
+      <DialogContent className=""></DialogContent>
+    </Dialog>
   );
 };
